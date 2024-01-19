6d23fcef39e25cf3e7263ea668f4f4fa7f8c6139
James Nelson <jamesnelson@deephaven.io>
2024-01-19 17:04:12 -0600

    fix: get sed on mac happy

--- tools/update_version.sh
+++ tools/update_version.sh
@@ -102,7 +102,7 @@ function update_file() {
     local extra="${4:-}"
     local expected="${prefix}${version}${extra}${suffix}"
     if ! grep -q "$expected" "$ROOT_DIR/plugins/$file"; then
-        sed "s/${prefix}.*/${expected}/g" -i "$ROOT_DIR/plugins/$file"
+        sed -e "s/${prefix}.*/${expected}/g" -i '' "$ROOT_DIR/plugins/$file"
         git add "$ROOT_DIR/plugins/$file"
         need_commit=true
     fi
